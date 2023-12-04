5bc358a303badc74dba00879cc10c50278303f61
Matthew Chee <matthew.chee@student.manchester.ac.uk>
2023-12-04 14:43:28 +0000

    Created heatmap with popups saying info

--- server/components/heatmap.py
+++ server/components/heatmap.py
@@ -1,59 +1,34 @@
-import folium
-from folium.plugins import HeatMap
 import pandas as pd
-import numpy as np
+import folium
+from folium.plugins import HeatMap, MarkerCluster
 
-# Define the place names, latitudes, and longitudes
-place_names = [
-    "University of Warwick, the UK",
-    "University of Bristol, the UK",
-    "London School of Economics and Political Science, the UK",
-    "King's College London, the UK",
-    "University of Manchester, the UK",
-    "University of Cambridge, the UK"
-]
+# Read data from CSV file
+file_path = '..\data\data.csv'  # Replace with your CSV file path
+df = pd.read_csv(file_path)
 
-latitudes = [
-    52.379414,
-    51.458447,
-    51.514450,
-    51.511448,
-    53.466843,
-    52.204311
-]
+# Calculate weighted averages
+df['Weighted Average'] = (4 * df['4*'] + 3 * df['3*'] + 2 * df['2*'] + 1 * df['1*']) / 100
 
-longitudes = [
-    -1.561904,
-    -2.603288,
-    -0.116674,
-    -0.116414,
-    -2.234597,
-    0.113818
-]
-
-# Generate arbitrary weighted averages for these universities
-# Using numpy to generate random numbers for demonstration purposes
-weighted_averages = np.random.uniform(1, 5, size=len(place_names))
-
-# Create the DataFrame
-uni_df = pd.DataFrame({
-    'Place Name': place_names,
-    'Latitude': latitudes,
-    'Longitude': longitudes,
-    'Weighted Average': weighted_averages
-})
-
-# Display the DataFrame
-print(uni_df)
-# Assuming you have a DataFrame 'df' with 'latitude', 'longitude', and 'weighted_average' columns
 # Create a base map
 map = folium.Map(location=[55.3781, -3.4360], zoom_start=6)  # Coordinates roughly at the center of the UK
 
 # Create a HeatMap data
-heat_data = [[row['Latitude'], row['Longitude'], row['Weighted Average']] for index, row in uni_df.iterrows()]
-
+heat_data = [[row['Latitude'], row['Longitude'], row['Weighted Average']] for index, row in df.iterrows()]
 # Add the heat map layer
 HeatMap(heat_data).add_to(map)
+
+# Create a MarkerCluster object
+marker_cluster = MarkerCluster().add_to(map)
+
+for index, row in df.iterrows():
+    # Create a marker with popup
+    popup_content = f"""<b>Location:</b> {row['Institution name']}<br>
+                        <b>Environment Score:</b> {row['Weighted Average']}<br>"""  # Replace 'some_info_column' with your column name
+    folium.Marker(
+        location=[row['Latitude'], row['Longitude']],
+        popup=folium.Popup(popup_content, max_width=300,min_width=100),
+        icon=None  # This will use the default icon
+    ).add_to(marker_cluster)
 
 # Save to an HTML file
 map.save('uk_heatmap.html')
