70b811a0a3813073b8f344c65025d319457f2cf7
Matthew Chee <matthew.chee@student.manchester.ac.uk>
2023-12-04 15:26:46 +0000

    Changed to image so works with component bubbles

--- server/components/scoreDistributionPie.py
+++ server/components/scoreDistributionPie.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import mpld3
 
 # Load data
 file_path = '..\data\data.csv'  # Replace with your CSV file path
@@ -9,11 +8,15 @@ df = pd.read_csv(file_path)
 # Calculate the sum of all the star ratings
 ratings = df[['4*', '3*', '2*', '1*']].sum()
 
-# Create the pie chart
-fig, ax = plt.subplots()
-ax.pie(ratings, labels=ratings.index, autopct='%1.1f%%', startangle=90)
-ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+# Set the size of the image - for example, 200x200 pixels
+figsize_in_inches = (200/96, 200/96)  # Convert pixels to inches for Matplotlib
+
+# Create the pie chart with the specified size
+plt.figure(figsize=figsize_in_inches)
+plt.pie(ratings, labels=ratings.index, autopct='%1.1f%%', startangle=90)
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 plt.title('Distribution of Star Ratings')
 
-# Save the plot as an HTML file using mpld3
-mpld3.save_html(fig, 'star_ratings_distribution.html')
+# Save the plot as a PNG image
+plt.savefig('star_ratings_distribution.png', bbox_inches='tight', dpi=96)
+plt.close()
